a8f957665f476f46f9c4bffda265e57756ae1733
Izzy Huang <66451797+IzzyHuang215@users.noreply.github.com>
2025-04-17 15:58:47 -0400

    4 direction control complete

--- 4_direction_control.c
+++ 4_direction_control.c
@@ -8,13 +8,14 @@
  #define F_CPU 16000000UL
 
  #include <avr/io.h>
+ //#include <avr/portmux.h>
  #include <xc.h>
  #include <util/delay.h>
  
  void setup_motorA() {
      DDRB |= (1 << DDB1) | (1 << DDB2);
  
-     TCCR1A |= (1 << COM1A1) | (1 << COM1B1); 
+     TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
      TCCR1A |= (1 << WGM11);
      TCCR1B |= (1 << WGM12) | (1 << WGM13);
  
@@ -27,36 +28,26 @@
  }
  
  void setup_motorB() {
-     DDRD |= (1 << DDD0) | (1 << DDD2);
+     DDRD |= (1 << DDD0) | (1 << DDD4);
  
-     TCCR3A = 0;
-     TCCR3B = 0;
- 
-     TCCR3A |= (1 << COM3A1) | (1 << COM3B1) | (1 << WGM31); 
-     TCCR3B |= (1 << WGM32) | (1 << WGM33);
- 
-     // Prescaler = 64
-     TCCR3B |= (1 << CS31) | (1 << CS30);
+     TCCR3A = (1 << COM3A1) | (1 << WGM31);
+     TCCR3B = (1 << WGM32) | (1 << WGM33) | (1 << CS31) | (1 << CS30);
  
      ICR3 = 255;
- 
-     // Start motor off
      OCR3A = 0;
-     OCR3B = 0;
  }
  
  void setup_motorC() {
-
      DDRD |= (1 << DDD1) | (1 << DDD3);
  
      TCCR4A = 0;
      TCCR4B = 0;
      TCCR4A |= (1 << COM4A1) | (1 << WGM41);
      TCCR4B |= (1 << WGM42) | (1 << WGM43);
-     TCCR4B |= (1 << CS41) | (1 << CS40); // Prescaler = 64
+     TCCR4B |= (1 << CS41) | (1 << CS40); 
  
-     ICR4 = 255;    
-     OCR4A = 0;  
+     ICR4 = 255;
+     OCR4A = 0; 
  }
  
  void move_backward(uint8_t duty) {
@@ -76,6 +67,29 @@
      OCR1B = 0;    
  }
  
+ void move_right() {
+     OCR1A = 0;
+     OCR1B = 49;
+ 
+     PORTD |= (1 << PD4);  
+     OCR3A = 255 - 221;
+ 
+     PORTD |= (1 << PD3);    
+     OCR4A = 255 - 195;       
+ }
+ 
+ void move_left() {
+     OCR1A = 204;
+     OCR1B = 0;
+ 
+     PORTD &= ~(1 << PD4);
+     OCR3A = 221;
+     
+     //motor 3
+     PORTD &= ~(1 << PD3);    
+     OCR4A = 49;
+ }
+ 
  void stop_motors_AC() {
      OCR1A = 0;
      OCR1B = 0;
@@ -91,18 +105,14 @@
      setup_motorC();
  
      while (1) {
-         //moving forward full speed
          move_forward(255);
-         _delay_ms(2000);
-         stop_motors_AC();
-         _delay_ms(1500);
-         
-         //moving backward full speed
+         _delay_ms(5000);
          move_backward(255);
-         _delay_ms(2000);
-         stop_motors_AC();
-         _delay_ms(1500);
+         _delay_ms(5000);
+         move_left(255);
+         _delay_ms(5000);
+         move_right(255);
+         _delay_ms(5000);
      }
-     return 0;
  }
  
